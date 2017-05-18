e7de3a17d2cdd8976d402d3e0815373f71fc523d
Tomer Saban <sab30226@gmail.com>
2017-05-18 15:24:18 +0300

    svm: Removed cutting of training data

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -23,10 +23,6 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
 #########################################################
-#Cutting training dataset
-features_train = features_train[:len(features_train)/100]
-labels_train = labels_train[:len(labels_train)/100]
-
 for c_param in (10, 100, 1000, 10000):
     svm = SVC(kernel='rbf', C=c_param)
 
