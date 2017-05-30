306eaf6c5ba0ea6d4f29c70a77650ef2b1223b95
Tomer Saban <sab30226@gmail.com>
2017-05-31 00:03:25 +0300

    outlier: Added code to print outlier

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -12,8 +12,26 @@ data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r")
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
-
+for k, i in data_dict.items():
+    try:
+        salary = int(i["salary"])
+        if salary > 2.5 * 10 ** 7:
+            print k
+    except:
+        pass
 ### your code below
+# from sklearn.linear_model import LinearRegression
+# reg = LinearRegression()
+# reg.fit(ages_train, net_worths_train)
+#
+# print "score: %f" % reg.score(ages_test, net_worths_test)
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
 
 
